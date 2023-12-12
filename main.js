59d59c88afc77fadc4ab093245cf58996ad16905
ovaqvado <likeriklohhg@gmail.com>
2023-12-12 19:37:09 +0300

    button

---
+++ main.js
@@ -0,0 +1,10 @@
+let newWindow;
+
+function openNewWindow() {
+    newWindow = window.open('https://itstep.org', 'width=640, height=480');
+}
+
+function closeWindow() {
+    newWindow.close();
+}
+    
